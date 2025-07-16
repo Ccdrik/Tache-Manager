385e644d530f898531da5ef9e906d1e808c60d7c
Ccdrik <cedric.claire03@gmail.com>
2025-07-16 15:46:57 +0200

    Mise en place AdminGuard + page 403

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,16 +9,17 @@ import { LoginComponent } from './auth/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
 import { UnauthorizedComponent } from './auth/403.component';
 import { AdminGuard } from './auth/admin.guard';
+import { AdminComponent } from './admin/admin.component';
 
 // Définition des routes de l'application
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirection vers la page de connexion par défaut
     {
         path: '',
-        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) // Chargement paresseux du module d'authentification
     },
-    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }, // Accès protégé par AuthGuard
     { path: 'taches', component: TachesComponent, canActivate: [AuthGuard] },
     { path: 'taches/ajouter', component: TaskCreateComponent, canActivate: [AuthGuard] },
     { path: 'taches/modifier/:id', component: TaskEditComponent, canActivate: [AuthGuard] },
@@ -26,6 +27,7 @@ export const routes: Routes = [
     { path: 'register', component: RegisterComponent }, //  accès public
     { path: 'login', component: LoginComponent },       //  accès public
     { path: 'task-list', component: TaskListComponent, canActivate: [AuthGuard] },
-    { path: '403', component: UnauthorizedComponent },
+    { path: '403', component: UnauthorizedComponent },  // Page d'erreur 403 pour accès non autorisé
+    { path: 'admin', component: AdminComponent, canActivate: [AuthGuard, AdminGuard] },
 
 ];
